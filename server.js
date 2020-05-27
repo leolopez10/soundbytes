ce77683694137a532c0e00dbc19b2bb048d19fda
lil L <57925562+leolopez10@users.noreply.github.com>
2020-05-27 11:16:18 -0500

    set up server and environemnt

---
+++ server.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+// const expressValidator = require('express-validator');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+app.use(morgan('dev'));
+app.use(cookieParser());
+// app.use(expressValidator());
+
+//Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  //Send every other request to the React App
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+//Database Connection
+mongoose
+  .connect(process.env.MONGODB_URI || process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() =>
+    console.log('====================DATABASE CONNECTED====================')
+  )
+  .catch(error => console.log(error));
+
+//Server Listening
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
